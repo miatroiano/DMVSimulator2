281d4e7aa01e32f196df1dfce1610aadff335987
miatroiano <m11ssing1n4ction@gmail.com>
2022-03-31 15:41:20 -0400

    setup

---
+++ src/DmvSimulator.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class DmvSimulator {
+	public static void main(String []args) {
+		Random random = new Random();
+		int randomNumber = random.nextInt(100);
+		
+		
+		Boolean call = false; 
+		int numb = 1;
+		System.out.println("welcome to the DMV!");
+		System.out.println("you are number " + randomNumber + " please wait until your number is called");
+		while (call == false) {
+			
+			if (randomNumber == numb) {
+				System.out.println(randomNumber);
+				call = true;
+				System.out.println("You dont have the required paperwork");
+				
+			}
+			else {
+				System.out.println(numb);
+				numb += 1;
+			}
+		}
+	}
+}
